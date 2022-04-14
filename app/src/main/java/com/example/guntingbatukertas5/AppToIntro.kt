932c373b82570c2c57b19b6f50babe5f90f489fc
zhavei <zhavei.vei@gmail.com>
2022-04-14 15:20:19 +0700

    first up

---
+++ app/src/main/java/com/example/guntingbatukertas5/AppToIntro.kt
@@ -0,0 +1,58 @@
+package com.example.guntingbatukertas5
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro
+import com.github.appintro.AppIntroCustomLayoutFragment.Companion.newInstance
+import com.github.appintro.AppIntroFragment
+
+class AppToIntro : AppIntro() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setBackgroundDrawable(ColorDrawable(Color.BLUE))
+        supportActionBar?.hide()
+        //layout was handle by library
+        addSlide(
+            AppIntroFragment.createInstance(
+                imageDrawable = R.drawable.ic_landing_page1,
+                description = "Bermain Suit Melawan Sesama Pemain"
+            )
+        )
+        addSlide(
+            AppIntroFragment.createInstance(
+                imageDrawable = R.drawable.ic_landing_page2,
+                description = "Bermain Suit Melawan Komputer"
+            )
+        )
+
+        ifUserSkiped()
+        showStatusBar(true)
+//        setStatusBarColorRes(R.color.orange)
+//        setNavBarColorRes(R.color.orange)
+        setProgressIndicator()
+
+    }
+
+//    override fun onSkipPressed(currentFragment: Fragment?) {
+//        super.onSkipPressed(currentFragment)
+//        onNextSlide()
+//    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        toMainActivty()
+    }
+
+    private fun ifUserSkiped() {
+        addSlide(newInstance(R.layout.intro_custom_layout1))
+    }
+
+    private fun toMainActivty() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/guntingbatukertas5/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.guntingbatukertas5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, AppToIntro::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2500)
+    }
+}
