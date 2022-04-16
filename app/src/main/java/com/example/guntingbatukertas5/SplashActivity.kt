cb3701af560527ee4ef6ee51c2e714ccd8601471
zhavei <zhavei.vei@gmail.com>
2022-04-16 22:43:03 +0700

    add glide

--- app/src/main/java/com/example/guntingbatukertas5/SplashActivity.kt
+++ app/src/main/java/com/example/guntingbatukertas5/SplashActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+
+        val imageView : ImageView = findViewById(R.id.splash_screen1)
+        Glide.with(this).load("https://i.ibb.co/HC5ZPgD/splash-screen1.png").into(imageView)
 
         Handler().postDelayed({
             val mainIntent = Intent(this, AppToIntro::class.java)
